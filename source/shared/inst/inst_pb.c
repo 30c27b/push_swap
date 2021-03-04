f7626ca2c5de34b8e6c84db0c026734e342a789c
Antoine Coulon <antoine@30c27b.com>
2021-03-04 13:16:16 +0100

    implemented instrctions pa and pb

--- source/shared/inst/inst_pb.c
+++ source/shared/inst/inst_pb.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inst_pa.c                                          :+:      :+:    :+:   */
+/*   inst_pb.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/04 12:59:42 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/04 13:14:47 by ancoulon         ###   ########.fr       */
+/*   Created: 2021/03/04 13:14:59 by ancoulon          #+#    #+#             */
+/*   Updated: 2021/03/04 13:15:45 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shared.h"
 
-void	inst_pa(t_stack *st_a, t_stack *st_b)
+void	inst_pb(t_stack *st_a, t_stack *st_b)
 {
-	if (st_b->len < 1)
+	if (st_a->len < 1)
 		return ;
-	st_a->data[st_a->len] = st_b->data[st_b->len - 1];
-	st_b->data[st_b->len - 1] = 0;
-	st_b->len -= 1;
-	st_a->len += 1;
+	st_b->data[st_b->len] = st_a->data[st_a->len - 1];
+	st_a->data[st_a->len - 1] = 0;
+	st_a->len -= 1;
+	st_b->len += 1;
 }
