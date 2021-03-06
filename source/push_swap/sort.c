10e470828ad75a0e1af6ef89e37a6f4908ef8f32
Antoine Coulon <antoine@30c27b.com>
2021-03-06 14:38:24 +0100

    finished sort function

--- source/push_swap/sort.c
+++ source/push_swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 13:57:40 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/06 13:58:25 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/03/06 14:37:18 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,18 @@
 
 void	ps_sort(t_stack *st_a, t_stack *st_b, size_t n)
 {
-	
+	if (n == 2 || n == 3)
+		if (st_a->data[st_a->len - 1] > st_a->data[st_a->len - 2])
+			inst_sa(st_a, st_b);
+	if (n == 3)
+	{
+		if (st_a->data[st_a->len - 3] < st_a->data[st_a->len - 2])
+		{
+			inst_pb(st_a, st_b);
+			inst_sa(st_a, st_b);
+			inst_pa(st_a, st_b);
+			if (st_a->data[st_a->len - 1] > st_a->data[st_a->len - 2])
+				inst_sa(st_a, st_b);
+		}
+	}
 }
