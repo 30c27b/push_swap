9637eed96356bfe1cd6221c58304c0a33532afda
ancoulon <ancoulon@student.s19.be>
2021-05-11 17:34:27 +0200

    stack5: case 3 wip

--- source/push_swap/stack5.c
+++ source/push_swap/stack5.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 19:39:47 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/05/11 16:50:52 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/05/11 17:33:51 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,27 @@ static int rotate_stack(t_stack *st_a, t_stack *st_b, t_llst **insts, int64_t ta
 	int		br;
 
 	i = 0;
-	// while (st_a->data[st_a->len - 1] < target && i++ < 3)
-	// {
-	// 	if (inst_save("ra", insts, st_a, st_b))
-	// 		return (1);
-	// }
 	len = st_a->len;
 	br = 0;
-	if (st_a->data[0] <= target)
+	if (st_a->data[0] <= target) // case 1: target > biggest element of stack a
 	{
 		if (inst_save("pa", insts, st_a, st_b))
 			return (1);
 		if (inst_save("ra", insts, st_a, st_b))
 			return (1);
 	}
-	if (st_a->data[st_a->len - 1] >= target)
+	else if (st_a->data[st_a->len - 1] >= target) // case 2: target < smallest element of stack a
 	{
+		if (inst_save("pa", insts, st_a, st_b))
+			return (1);
+	}
+	else // TODO: case 3: target inside the stack a, iterate in stack to find the index of target
+	{
+		while (i < len - 1 && !(st_a->data[i] <= target))
+			i++;
+		while (i-- > 0)
+			if (inst_save("rra", insts, st_a, st_b))
+			return (1);
 		if (inst_save("pa", insts, st_a, st_b))
 			return (1);
 	}
