32f4b3b164a19f11ada0481d0052215b16903352
ancoulon <ancoulon@student.s19.be>
2021-05-12 16:31:21 +0200

    having a bad time on stack5

--- source/push_swap/stack3.c
+++ source/push_swap/stack3.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 19:39:47 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/05/11 12:10:15 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/05/12 14:27:49 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	ps_stack3(t_stack *st_a, t_stack *st_b, t_llst **insts)
 	// 321 -> 123
 	if (st_a->data[2] > st_a->data[1] && st_a->data[1] > st_a->data[0])
 	{
-		printf("case 1\n");
 		if (inst_save("sa", insts, st_a, st_b))
 			return (1);
 		if (inst_save("rra", insts, st_a, st_b))
@@ -28,21 +27,18 @@ int	ps_stack3(t_stack *st_a, t_stack *st_b, t_llst **insts)
 	// 312 -> 123
 	else if (st_a->data[2] > st_a->data[0] && st_a->data[1] < st_a->data[0])
 	{
-		printf("case 2\n");
 		if (inst_save("ra", insts, st_a, st_b))
 			return (1);
 	}
 	// 213 -> 123
 	else if (st_a->data[0] > st_a->data[2] && st_a->data[1] < st_a->data[2])
 	{
-		printf("case 3\n");
 		if (inst_save("sa", insts, st_a, st_b))
 			return (1);
 	}
 	// 132 -> 123
 	else if (st_a->data[1] > st_a->data[0] && st_a->data[2] < st_a->data[0])
 	{
-		printf("case 4\n");
 		if (inst_save("sa", insts, st_a, st_b))
 			return (1);
 		if (inst_save("ra", insts, st_a, st_b))
@@ -51,7 +47,6 @@ int	ps_stack3(t_stack *st_a, t_stack *st_b, t_llst **insts)
 	// 231 -> 123
 	else if (st_a->data[1] > st_a->data[2] && st_a->data[0] < st_a->data[2])
 	{
-		printf("case 5\n");
 		if (inst_save("rra", insts, st_a, st_b))
 			return (1);
 	}
--- source/push_swap/stack5.c
+++ source/push_swap/stack5.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 19:39:47 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/05/11 17:33:51 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/05/12 16:18:44 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@
 static int rotate_stack(t_stack *st_a, t_stack *st_b, t_llst **insts, int64_t target)
 {
 	size_t	i;
+	size_t	j;
 	size_t	len;
-	int		br;
 
 	i = 0;
+	j = 0;
 	len = st_a->len;
-	br = 0;
+	printf("===========\nrotate_stack starts with target %llu\n----------\n", target);
+	stack_print(st_a, st_b);
+	printf("-----\n");
 	if (st_a->data[0] <= target) // case 1: target > biggest element of stack a
 	{
 		if (inst_save("pa", insts, st_a, st_b))
@@ -37,14 +40,31 @@ static int rotate_stack(t_stack *st_a, t_stack *st_b, t_llst **insts, int64_t ta
 	}
 	else // TODO: case 3: target inside the stack a, iterate in stack to find the index of target
 	{
-		while (i < len - 1 && !(st_a->data[i] <= target))
+		// while (i < len - 1 && !(st_a->data[st_a->len - i - 1] > target))
+		// 	i++;
+		// printf("i = %zu\n",i);
+		// while (j++ < i)
+		// {
+		// 	printf("ra\n");
+		// 	inst_save("ra", insts, st_a, st_b);
+		// }
+		// j = 0;
+		while (st_a->data[st_a->len - 1] < target)
+		{
+			printf("rra\n");
 			i++;
-		while (i-- > 0)
-			if (inst_save("rra", insts, st_a, st_b))
-			return (1);
+			inst_save("rra", insts, st_a, st_b);
+		}
 		if (inst_save("pa", insts, st_a, st_b))
 			return (1);
+		while (j++ < i + 1)
+		{
+			printf("ra\n");
+			inst_save("ra", insts, st_a, st_b);
+		}
 	}
+	printf("----\nrotate_stack ends with target %llu\n===========\n", target);
+	stack_print(st_a, st_b);
 	return (0);
 }
 
@@ -64,8 +84,6 @@ int	ps_stack5(t_stack *st_a, t_stack *st_b, t_llst **insts)
 	while (i++ < push)
 	{
 		if (rotate_stack(st_a, st_b, insts, st_b->data[st_b->len - 1]))
-			return (1);
-		if (inst_save("pa", insts, st_a, st_b))
 			return (1);
 	}
 	return (0);
